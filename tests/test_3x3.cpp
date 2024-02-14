29ae7e03eff695dd570848d7eeb0ab9d470c7210
yakov7podkova <romannakova@yandex.com>
2024-02-15 00:37:57 +0300

    new 3x3 tests

--- tests/test_3x3.cpp
+++ tests/test_3x3.cpp
@@ -14,8 +14,8 @@ TEST(diagonal_3x3, HandlesAnswer) {
 
     vd ans = thomas_method(Mat, d);
     vd expected_ans = {7.0 / 9.0, 7.0 / 9.0, 7.0 / 9.0};
-
-    EXPECT_EQ(ans, expected_ans);
+    
+    for (std::size_t i = 0; i < ans.size(); i++) { EXPECT_DOUBLE_EQ(ans[i], expected_ans[i]); }
     
 };
 
@@ -30,17 +30,68 @@ TEST(symmetric_3x3, HandlesAnswer) {
 
     vd ans = thomas_method(Mat, d);
     vd expected_ans = {3.0 / 4.0, 1.0 / 2.0, 3.0 / 4.0};
+    
+    for (std::size_t i = 0; i < ans.size(); i++) { EXPECT_DOUBLE_EQ(ans[i], expected_ans[i]); }
 
-    EXPECT_EQ(ans[2], expected_ans[2]);
+};
+
+TEST(random_3x3, HandlesAnswer) {
+    
+    vd main = {8.0, 8.0, -9.0};
+    vd low = {0, -1.0, 6.0};
+    vd high = {4.0, 3.0, 0};
+
+    TDM Mat(main, low, high);
+
+    vd ans = thomas_method(Mat, d);
+    vd expected_ans = {7.0 / 18.0, 35.0 / 36.0, -7.0 / 54.0};
+
+    for (std::size_t i = 0; i < ans.size(); i++) { EXPECT_DOUBLE_EQ(ans[i], expected_ans[i]); }
+};
+
+TEST(random_3x3_harder, HandlesAnswer) {
+    
+    vd main = {8.2, 8.3, 9.2};
+    vd low = {0, 1.6, 5.7};
+    vd high = {4.6, 2.9, 0};
+
+    TDM Mat(main, low, high);
+
+    vd ans = thomas_method(Mat, d);
+    vd expected_ans = { 0.510648, 0.611453, 0.382034};
+
+    for (std::size_t i = 0; i < ans.size(); i++) { EXPECT_NEAR(ans[i], expected_ans[i], 0.000001); }
+
+};
+
+TEST(random_3x3_BigNumbers, HandlesAnswer) {
+
+    vd main = {88878.0, 83765.0, 925000.0};
+    vd low = {0.0, -168.0, 57965.0};
+    vd high = {4679.0, 2.9, 0.0};
+
+    TDM Mat(main, low, high);
+
+    vd ans = thomas_method(Mat, d);
+    vd expected_ans = { 0.0000743524, 0.0000837162, 0.00000232232};
+
+    for (std::size_t i = 0; i < ans.size(); i++) { EXPECT_NEAR(ans[i], expected_ans[i], 0.000001); }
 
 };
 
 
+TEST(random_3x3_IncorrectMatrix, HandlesAnswer) {
 
+    vd main = {8.0, 8.0, 1.0};
+    vd low = {0.0, -1.0, 6.0};
+    vd high = {4.0, 3.0, 0.0};
 
+    TDM Mat(main, low, high);
 
-//int main(int argc, char **argv) {
-//    ::testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
+    vd ans = thomas_method(Mat, d);
+    vd expected_ans = { 35.0 / 106.0, 231.0 / 212.0, -49.0 / 106.0};
 
-//};
+    for (std::size_t i = 0; i < ans.size(); i++) { EXPECT_NE(ans[i], expected_ans[i]); }
+
+};
+
